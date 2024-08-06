a373bb7562a695a03c0496001033141d40fe8f5c
Mahim Ali <117038967+Mahimali937@users.noreply.github.com>
2024-08-05 22:46:39 -0400

    Update CorsConfig.java

--- src/main/java/com/ll/la_liga_zone/config/CorsConfig.java
+++ src/main/java/com/ll/la_liga_zone/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://localhost:3000", "https://laligazone.vercel.app")
                         .allowedMethods("GET");
             }
         };
