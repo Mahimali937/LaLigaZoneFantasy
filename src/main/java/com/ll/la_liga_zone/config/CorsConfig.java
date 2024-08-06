d9487e73fab1ef53585a4b16fa5473b4bcc66670
Mahim Ali <117038967+Mahimali937@users.noreply.github.com>
2024-08-05 23:35:44 -0400

    Update CorsConfig.java

--- src/main/java/com/ll/la_liga_zone/config/CorsConfig.java
+++ src/main/java/com/ll/la_liga_zone/config/CorsConfig.java
@@ -7,16 +7,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
-
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
+                registry.addMapping("/api/**")
                         .allowedOrigins("http://localhost:3000", "https://laligazone.vercel.app")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*");
+                        .allowedMethods("GET");
             }
         };
     }
