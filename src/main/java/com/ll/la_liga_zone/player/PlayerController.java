f70c1b0a227b72c0499bd1a9af63e358f304b138
Mahim Ali <117038967+Mahimali937@users.noreply.github.com>
2024-08-05 23:13:43 -0400

    Update PlayerController.java

--- src/main/java/com/ll/la_liga_zone/player/PlayerController.java
+++ src/main/java/com/ll/la_liga_zone/player/PlayerController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin("https://laligazone.vercel.app")
 @RequestMapping(path = "api/v1/player")
 public class PlayerController {
 
